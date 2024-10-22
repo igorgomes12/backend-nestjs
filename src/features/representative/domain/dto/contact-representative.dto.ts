73bbc62e57cc9ceefc98160756ec391be5f0b7d2
Igor Gomes <94501528+igorgomes12@users.noreply.github.com>
2024-10-22 16:34:42 -0300

    feat(contact): ajuste de tipos

--- src/features/representative/domain/dto/contact-representative.dto.ts
+++ src/features/representative/domain/dto/contact-representative.dto.ts
@@ -1,20 +1,11 @@
-import { formatPhone, phoneRegex } from "@common/utils/regex/phone";
 import z from "zod";
 
 export const contactRepresentativeSchemaDto = z.object({
   cellphone: z.string(),
-  // .transform(formatPhone)
-  // .refine((phone) => phoneRegex.test(phone), {
-  //   message:
-  //     "Favor digitar o número do celular no formato correto (XX) XXXXX-XXXX",
-  // }),
   phone: z.string(),
-  // .transform(formatPhone)
-  // .refine((phone) => phoneRegex.test(phone), {
-  //   message:
-  //     "Favor digitar o número do telefone no formato correto (XX) XXXX-XXXX",
-  // }),
   email: z.string(),
+  description: z.string().optional(),
+  favorite: z.boolean().optional(),
 });
 export type ContactRepresentativeDto = z.infer<
   typeof contactRepresentativeSchemaDto
--- src/features/representative/domain/dto/create-representative.dto.ts
+++ src/features/representative/domain/dto/create-representative.dto.ts
@@ -7,7 +7,7 @@ export const createRepresentativeSchemaDto = z.object({
   name: z.string().min(3, { message: "Favor digitar o nome do representante" }),
   region: z.string().min(1, { message: "Favor digitar o nome da região" }),
   supervisor: z.string(),
-  status: z.enum(["ativo", "inativo"]).default("ativo"),
+  status: z.string(),
   type: z.enum(["REPRESENTATIVE", "CONSULTANT", "PARTHER"]),
   commission: commissionRepresentativeSchemaDto,
   contact: contactRepresentativeSchemaDto,
